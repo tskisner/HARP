c78e597beddefe863f53324e33f83278eef7654d
Theodore Kisner <tskisner.public@gmail.com>
2011-05-03 10:12:35 -0700

    Start adding toy image format for testing.

---
+++ instruments/toy/harp_toy.hpp
@@ -0,0 +1,26 @@
+// @COPYRIGHT@
+
+namespace harp {
+  
+  class image_toy : public image {
+    
+    public :
+      image_toy ( std::map < std::string, std::string > const & params );
+      ~image_toy ( );
+      
+      size_t rows ( ) { return rows_; }
+      size_t cols ( ) { return cols_; }
+      void rowscols ( size_t & rows, size_t & cols ) { rows = rows_; cols = cols_; return; }
+      void read ( size_t startrow, size_t startcol, harp::dense_mat_view & data );
+      void write ( std::string const & path, size_t startrow, size_t startcol, harp::dense_mat_view & data ) { }
+    
+    private :
+    
+      size_t rows_;
+      size_t cols_;
+      std::string path_;
+    
+  };
+  
+  
+}
---
+++ instruments/toy/harp_toy_image.cpp
@@ -0,0 +1,19 @@
+// @COPYRIGHT@
+
+#include <harp.hpp>
+
+
+using namespace std;
+using namespace harp;
+
+
+harp::toy_image::toy_image ( std::map < std::string, std::string > const & params ) {
+  
+  
+}
+
+
+harp::toy_image::~toy_image ( ) {
+  
+  
+}
