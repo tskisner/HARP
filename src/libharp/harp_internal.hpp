c413301f2b36a43d44d78dede4dce68068d940a6
Theodore Kisner <tskisner.public@gmail.com>
2011-05-03 13:39:22 -0700

    forgot to commit internal header

---
+++ src/libharp/harp_internal.hpp
@@ -0,0 +1,17 @@
+// @COPYRIGHT@
+
+#ifndef HARP_INTERNAL_HPP
+#define HARP_INTERNAL_HPP
+
+#include <config.h>
+#include <harp.hpp>
+
+namespace harp {
+  
+  
+}
+
+
+#include "instruments.hpp" 
+  
+#endif
